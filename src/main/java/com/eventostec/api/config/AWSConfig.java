f4746526e761ae504850003d64fe42ffaf62d068
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2024-06-21 00:38:44 -0300

    fix: changing AmazonS3 configuration to S3Client

--- src/main/java/com/eventostec/api/config/AWSConfig.java
+++ src/main/java/com/eventostec/api/config/AWSConfig.java
@@ -1,11 +1,10 @@
 package com.eventostec.api.config;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AWSConfig {
@@ -14,11 +13,9 @@ public class AWSConfig {
     private String awsRegion;
 
     @Bean
-    public AmazonS3 createS3Instance(){
-        return AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
-                .withRegion(awsRegion)
+    public S3Client createS3Instance() {
+        return S3Client.builder()
+                .region(Region.of(awsRegion))
                 .build();
     }
 }
