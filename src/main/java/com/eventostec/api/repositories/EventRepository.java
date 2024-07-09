f0d6daa5ccc1ae20274c054f4e527ef2593f6959
Fernanda <nanda.kipper@gmail.com>
2024-07-08 22:53:00 -0300

    feat: add left join to getUpcomingEvents

--- src/main/java/com/eventostec/api/repositories/EventRepository.java
+++ src/main/java/com/eventostec/api/repositories/EventRepository.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
     @Query("SELECT e.id AS id, e.title AS title, e.description AS description, e.date AS date, e.imgUrl AS imgUrl, e.eventUrl AS eventUrl, e.remote AS remote, a.city AS city, a.uf AS uf " +
-            "FROM Event e JOIN Address a ON e.id = a.event.id " +
+            "FROM Event e LEFT JOIN Address a ON e.id = a.event.id " +
             "WHERE e.date >= :currentDate")
     public Page<EventAddressProjection> findUpcomingEvents(@Param("currentDate") Date currentDate, Pageable pageable);
 
